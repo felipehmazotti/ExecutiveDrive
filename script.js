0b894187b6da048034a8fa3275c5b04ef4cd4c5b
Cauã Lucas <cauallucas71@gmail.com>
2023-09-27 16:44:56 -0300

    Update-c

---
+++ script.js
@@ -0,0 +1,11 @@
+// propaganda
+
+function mostrarPopup() {
+    popup.style.display = 'flex';
+}
+
+function fecharPopup() {
+    popup.style.display = 'none';
+}
+
+setTimeout(mostrarPopup, 2000);
